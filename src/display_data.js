a2e2fec986abbde9a1c2b4e16669568a682d91d6
jyothikiran843 <jyothikiranthammana843@gmail.com>
2023-08-06 16:23:47 +0530

    changes to preview

--- src/display_data.js
+++ src/display_data.js
@@ -6,15 +6,17 @@ export default function DisplayData({ldata}){
     const data=ldata.values;
     const [previewModalShow, setPreviewModalShow] = useState(false);
     const [selectedDriveId, setSelectedDriveId] = useState('');
+    const [selectedDriveData,setSelectedDriveData]=useState('');
   
-    const openPreviewModal = (driveId) => {
-      setSelectedDriveId(driveId);
-      setPreviewModalShow(true);
+    function openPreviewModal(driveId,row){
+        setSelectedDriveData(row);
+        setSelectedDriveId(driveId);
+        setPreviewModalShow(true);
     };
   
-    const closePreviewModal = () => {
-      setSelectedDriveId('');
-      setPreviewModalShow(false);
+    function closePreviewModal(){
+        setSelectedDriveId('');
+        setPreviewModalShow(false);
     };
     console.log(data);
 
@@ -31,18 +33,18 @@ export default function DisplayData({ldata}){
           {data.map((row, index) => (
             <Card style={{width:'18rem'}} className='col-md-2 m-2' key={index}>
                 <Card.Img varient='top' src={`https://drive.google.com/thumbnail?id=${row[6]}`} />
-              <Card.Body>
-                <Card.Title>From: {row[0]}</Card.Title>
+              <Card.Body className='text-center'>
+                <Card.Title> {row[0]}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{row[1]}</Card.Subtitle>
-                <Card.Text>Year Conducted: {row[2]}</Card.Text>
-                <Card.Text>{row[3]}</Card.Text>
-                <Card.Text>{row[4]}</Card.Text>
-                <Card.Text>{row[5]}</Card.Text>
-                <Card.Text>{row[8]}</Card.Text>
-                <Button variant="primary" onClick={() => openPreviewModal(row[6])}>
+                <Card.Text>Year Conducted: <b>{row[2]}</b></Card.Text>
+                <Card.Text>Subject: <b>{row[3]}</b></Card.Text>
+                <Card.Text>Type: <b>{row[4]}</b></Card.Text>
+                <Card.Text>Time Uploaded: <b>{row[5]}</b></Card.Text>
+                <Card.Text>By: <b>{row[8]}</b></Card.Text>
+                <Button variant="primary" onClick={() => openPreviewModal(row[6],row)}>
                   Preview
                 </Button>
-                <Button variant="secondary" onClick={() => downloadFile(row[6])}>Download</Button>
+                <Button className='m-2' variant="secondary" onClick={() => downloadFile(row[6])}>Download</Button>
               </Card.Body>
             </Card>
           ))}
@@ -50,11 +52,11 @@ export default function DisplayData({ldata}){
   
         <Modal fullscreen={true} show={previewModalShow} onHide={closePreviewModal} centered>
           <Modal.Header closeButton>
-            <Modal.Title>Preview for</Modal.Title>
+            <Modal.Title>{`Preview for ${selectedDriveData[0]} ${selectedDriveData[1]} ${selectedDriveData[2]} ${selectedDriveData[3]} ${selectedDriveData[5]}`}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <iframe
-                title="Drive Preview"
+                title={`Drive Preview for File: ${selectedDriveData[0]} ${selectedDriveData[1]} ${selectedDriveData[2]} ${selectedDriveData[3]} ${selectedDriveData[5]}`}
                 src={`https://drive.google.com/file/d/${selectedDriveId}/preview`}
                 width="100%"
                 height="100%"
